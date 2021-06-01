ed5ab7610d7938bb47e45daa639e9792c9845556
Bojana <bojanabojic30@gmail.com>
2021-06-01 16:26:28 +0200

    The user can select the display mode

--- src/components/displayOption/index.js
+++ src/components/displayOption/index.js
@@ -1,13 +1,30 @@
 import React from "react";
-import List from "../list";
-import Card from "./../card";
+import { makeStyles } from "@material-ui/core/styles";
+import ViewModuleIcon from "@material-ui/icons/ViewModule";
+import IconButton from "@material-ui/core/IconButton";
+import ViewListIcon from "@material-ui/icons/ViewList";
+//import FormatListBulletedIcon from "@material-ui/icons/FormatListBulleted";
+// import List from "../list";
+// import Card from "./../card";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 function DisplayOption() {
+  const classes = useStyles();
   return (
-    <div>
-      <Card />
-      or
-      <List />
+    <div className={classes.root}>
+      <IconButton>
+        <ViewModuleIcon />
+      </IconButton>
+      <IconButton>
+        <ViewListIcon />
+      </IconButton>
     </div>
   );
 }
