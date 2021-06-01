6645618ae541d7d90cfc0c8f7ee54bbfeaaf1af2
Bojana <bojanabojic30@gmail.com>
2021-06-01 15:56:22 +0200

    Add input component

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,6 +1,22 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+}));
 
 function Input() {
-  return <form>Input</form>;
+  const classes = useStyles();
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <TextField id="standard-basic" label="Search topic" />
+    </form>
+  );
 }
 export default Input;
